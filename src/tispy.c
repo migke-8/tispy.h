064da84e6d6f9d03606b3681562d28d69fe52b15
migke-8 <miguelportelabispo@gmail.com>
2025-12-27 13:34:24 -0300

    update

--- src/tispy.c
+++ src/tispy.c
@@ -1,13 +1,24 @@
-#ifndef TINY_LISP
-#define TINY_LISP
+#ifndef TISPY
+#define TISPY
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define MAX_TOKEN_SIZE 2048
-#define MAX_STACK_SIZE 16 * 16 * 16 * 16
-typedef enum TokenType { STATEMENT = 1, EXPRESSION } TokenType;
+#define DATA_STRIPE 40
+#define INITIAL_HEAP_SIZE 32
+typedef enum {
+  STATEMENT_FUNCTION = 1,
+  EXPRESSION_BOOLEAN,
+  EXPRESSION_STRING,
+  EXPRESSION_INTEGER,
+  EXPRESSION_DOUBLE,
+  EXPRESSION_TABLE,
+  EXPRESSION_FUNCTION,
+  EXPRESSION_UNDEFINED
+} Type;
 typedef struct Token {
-  TokenType type;
+  Type type;
   char *content;
 } Token;
 typedef struct TreeNode {
@@ -15,18 +26,66 @@ typedef struct TreeNode {
   struct TreeNode *branch;
   Token token;
 } TreeNode;
-static void logError(char *content) { printf("[ERROR]:\n%s\n", content); }
-TreeNode *parse(FILE *file) {
+typedef struct Data {
+  Type type;
+  unsigned char value[DATA_STRIPE];
+  struct Data *next;
+} Data;
+typedef struct {
+  int size;
+  Data table[];
+} DataTable;
+typedef struct {
+  TreeNode *root;
+  DataTable *heap;
+  Data *stack;
+} TispyState;
+typedef enum { FILE_INPUT = 1, STRING_INPUT } InputKind;
+typedef struct {
+  InputKind kind;
+  struct {
+    FILE *file;
+    char *string;
+  } data;
+} Input;
+static Data* popStack() {
+}
+static char igetc(Input *i) {
+  return i->kind == FILE_INPUT ? fgetc(i->data.file) : i->data.string[0]++;
+}
+static void logError(char *content) {
+  printf("Tispy interpreter:\n[ERROR]: %s\n", content);
+}
+static int isInt(char *content) {
+  for (int i = 0; i < strlen(content); i++) {
+    if (!isdigit(content[i]))
+      return 0;
+  }
+  return 1;
+}
+static Type getType(char *content) {
+  if (!strcmp(content, "true") || !strcmp(content, "false")) {
+    return EXPRESSION_BOOLEAN;
+  }
+  if (content[0] == '"' && content[strlen(content)] == '"') {
+    return EXPRESSION_STRING;
+  }
+  if (isInt(content)) {
+    return EXPRESSION_INTEGER;
+  }
+  return EXPRESSION_UNDEFINED;
+}
+TreeNode *parse(Input *i) {
   char character = ' ';
   char currentContent[MAX_TOKEN_SIZE];
   int currentContentSize = 0;
   TreeNode *result = malloc(sizeof(TreeNode));
-  TreeNode *branchesStack[MAX_STACK_SIZE];
   TreeNode *currentNode = result;
+  int branchesStackSize = 16;
+  TreeNode **branchesStack = calloc(branchesStackSize, sizeof(TreeNode));
   int openCount = 0;
   int firstVisits = 0;
-  branchesStack[0] = result;
-  while ((character = fgetc(file)) != EOF) {
+  while ((character = igetc(i)) != EOF) {
     printf("char = %s\n", &character);
     if (character == ' ' && firstVisits < openCount) {
       TreeNode *newNode = malloc(sizeof(TreeNode));
@@ -35,19 +94,22 @@ TreeNode *parse(FILE *file) {
       newNode->branch = NULL;
       newNode->children = NULL;
       if (firstVisits < openCount) {
-        if (firstVisits >= MAX_STACK_SIZE) {
-          logError("parse error: theoretical stack size limit exceeded.");
-          return NULL;
+        if (firstVisits >= branchesStackSize) {
+          TreeNode **oldStack = branchesStack;
+          branchesStackSize *= 2;
+          branchesStack = malloc(branchesStackSize);
+          memcpy(branchesStack, oldStack, branchesStackSize / 2);
+          free(oldStack);
         }
         branchesStack[firstVisits] = newNode;
         currentNode->branch = newNode;
-        currentNode->branch->token.type = STATEMENT;
+        currentNode->branch->token.type = STATEMENT_FUNCTION;
         currentNode = currentNode->branch;
         firstVisits++;
         continue;
       }
       currentNode->children = newNode;
-      currentNode->branch->token.type = EXPRESSION;
+      currentNode->branch->token.type = getType(currentContent);
       currentNode = currentNode->children;
       continue;
     }
@@ -71,16 +133,19 @@ TreeNode *parse(FILE *file) {
   }
   return currentNode;
 }
-TreeNode *evaluate(TreeNode *root) {
+TreeNode *evaluate(TispyState ctx, TreeNode *root, char *funcName) {
   if (root == NULL) {
     return root;
   }
-  TreeNode *evaluated = evaluate(root->children);
-  if (root->token.type == STATEMENT) {
-    if (strcmp(root->token.content, "car")) {
-      return root;
-    } else if (strcmp(root->token.content, ))
+  char *content = root->token.content;
+  TreeNode *evaluated = evaluate(ctx, root->children, content);
+  if (root->token.type == STATEMENT_FUNCTION) {
+    if (strcmp(content, "print")) {
+    }
+  } else {
+    if (strcmp(funcName, "print")) {
+    }
   }
-  return evaluate(root->branch);
+  return evaluate(ctx, root->branch, funcName);
 }
 #endif
